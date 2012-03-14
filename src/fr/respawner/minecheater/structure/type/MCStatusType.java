58964f2c0a14037e0e21c0720ed885b4ead3810a
Guillaume Mazoyer <respawneral@gmail.com>
2012-03-14 20:27:41 +0100

    Now the code is under the MIT license.

--- src/fr/respawner/minecheater/structure/type/MCStatusType.java
+++ src/fr/respawner/minecheater/structure/type/MCStatusType.java
@@ -1,44 +1,66 @@
+/*
+ * Copyright (c) 2012 Guillaume Mazoyer
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package fr.respawner.minecheater.structure.type;
 
 public enum MCStatusType {
-        UNKNOWN, HURT, DEAD, TAMING, TAMED, SHAKING_WATER, EATING;
+    UNKNOWN, HURT, DEAD, TAMING, TAMED, SHAKING_WATER, EATING;
 
-        public static MCStatusType statusForID(byte id) {
-            switch (id) {
-            case 2:
-                return HURT;
-            case 3:
-                return DEAD;
-            case 6:
-                return TAMING;
-            case 7:
-                return TAMED;
-            case 8:
-                return SHAKING_WATER;
-            case 9:
-                return EATING;
-            default:
-                return UNKNOWN;
-            }
+    public static MCStatusType statusForID(byte id) {
+        switch (id) {
+        case 2:
+            return HURT;
+        case 3:
+            return DEAD;
+        case 6:
+            return TAMING;
+        case 7:
+            return TAMED;
+        case 8:
+            return SHAKING_WATER;
+        case 9:
+            return EATING;
+        default:
+            return UNKNOWN;
         }
+    }
 
-        @Override
-        public String toString() {
-            switch (this) {
-            case HURT:
-                return "Hurt";
-            case DEAD:
-                return "Dead";
-            case TAMING:
-                return "Taming";
-            case TAMED:
-                return "Tamed";
-            case SHAKING_WATER:
-                return "Shaking water off itself";
-            case EATING:
-                return "Eating";
-            default:
-                return "Unknown";
-            }
+    @Override
+    public String toString() {
+        switch (this) {
+        case HURT:
+            return "Hurt";
+        case DEAD:
+            return "Dead";
+        case TAMING:
+            return "Taming";
+        case TAMED:
+            return "Tamed";
+        case SHAKING_WATER:
+            return "Shaking water off itself";
+        case EATING:
+            return "Eating";
+        default:
+            return "Unknown";
         }
+    }
 }
